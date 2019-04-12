83544362adf28bce4092236bafa13186166be782
ninjapretzel <ninjapretzel@yahoo.com>
2019-04-12 14:20:10 -0600

    Makes packing/unpacking functions for Base64

--- ExServer/Core/Utils/Packing.cs
+++ ExServer/Core/Utils/Packing.cs
@@ -7,16 +7,51 @@ using System.Runtime.InteropServices;
 using System.Runtime.CompilerServices;
 
 namespace Ex.Utils {
-	public class Packing {
-		public static void Unpack<T>(string encoded, out T ret) where T : struct {
-			ret = default(T);
 
-			
+	/// <summary> Class holding code for packing structs </summary>
+	public static class Pack {
+
+		/// <summary> Pack a struct into a Base64 string </summary>
+		/// <typeparam name="T"> Generic type of parameter </typeparam>
+		/// <param name="value"> Parameter to pack </param>
+		/// <returns> Base64 from converting struct into byte[], then encoding byte array </returns>
+		public static string Base64<T>(T value) where T : struct {
+			byte[] copy = Unsafe.ToBytes(value);
+			return Convert.ToBase64String(copy);
 		}
 
-		public static string Pack<T>(T value) where T : struct {
-			byte[] copy = Unsafe.ToBytes(value);
-			return "";
+	}
+
+	/// <summary> Class holding code for unpacking structs </summary>
+	public static class Unpack {
+		
+		/// <summary> Unpack Base64 encoded struct into a struct by output parameter </summary>
+		/// <typeparam name="T"> Generic type of parameter to unpack </typeparam>
+		/// <param name="encoded"> Encoded Base64 string </param>
+		/// <param name="ret"> Return location </param>
+		/// <returns> True if successful, false if failure, and sets ret to the resulting unpacked data, or default, respectively.  </returns>
+		public static bool Base64<T>(string encoded, out T ret) where T : struct {
+			try {
+				byte[] bytes = Convert.FromBase64String(encoded);
+				ret = Unsafe.FromBytes<T>(bytes);
+				return true;
+			} catch (Exception) {
+				ret = default(T);
+				return false;
+			}
+		}
+
+		/// <summary> Unpack Base 64 </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="encoded"></param>
+		/// <returns></returns>
+		public static T Base64<T>(string encoded) where T : struct {
+			try {
+				byte[] bytes = Convert.FromBase64String(encoded);
+				return Unsafe.FromBytes<T>(bytes);
+			} catch (Exception) {
+				return default(T);
+			}
 		}
 
 	}

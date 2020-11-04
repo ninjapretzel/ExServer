41e5ddc9506bfe0be2b7016683d5493cbc5c992d
ninjapretzel <ninjapretzel@yahoo.com>
2020-11-04 13:43:12 -0700

    Adds `Breed()` to ArtBreederAPI, `Save()` accepts multiple parents.

--- Generator/ArtbreederAPI.cs
+++ Generator/ArtbreederAPI.cs
@@ -7,7 +7,18 @@ using System.Text;
 namespace Ex {
 	public class ArtbreederAPI {
 
+		/// <summary> Clamp <paramref name="v"/> between 0 and 1 </summary>
+		/// <param name="v"> value to clamp </param>
+		/// <returns> [ 0 ... v ... 1 ] </returns>
+		public static double Clamp01(double v) { return v < 0 ? 0 : v > 1 ? 1 : v; }
+		/// <summary> Clamp <paramref name="v"/> between <paramref name="min"/> and <paramref name="max"/> </summary>
+		/// <param name="v"> value to clamp </param>
+		/// <param name="min"> minimum value to return </param>
+		/// <param name="max"> maximum value to return </param>
+		/// <returns> [ min ... v ... max ] </returns>
+		public static double Clamp(double v, double min, double max) { return v < min ? min : v > max ? max : v; }
 
+		/// <summary> Information about Artbreeder's available models. </summary>
 		public static JsonObject models = Json.Parse(@"{
 	anime_portraits:{
 		magnitudes: { chaos: [ .1, 1.5 ], $rest: [ -3, 3 ], },
@@ -86,36 +97,56 @@ namespace Ex {
 		},
 	}
 }".Replace('\'', '\"')) as JsonObject;
+		/// <summary> Cookie to use when sending requests to Artbreeder. </summary>
 		public static string cookie = "";
 
+		public static readonly string baseURL = "https://artbreeder.com";
+		public static readonly string tempComposeURL = baseURL + "/tmp_compose_images";
+
+		/// <summary> Prepare a string into an HTTPContent when POSTing to Artbreeder </summary>
+		/// <param name="content"> String to prepare </param>
+		/// <returns> HTTP Content for request body </returns>
 		public static HttpContent Prepare(string content) {
 			HttpContent c = new StringContent(content, Encoding.UTF8, "application/json");
 			c.Headers.Add("Cookie", cookie);
 			return c;
 		}
 
-
+		/// <summary> Generate and save a new picture from the given model. </summary>
+		/// <param name="model"> Model to generate with </param>
 		public static void GenerateNew(string model) {
 			if (!models.ContainsKey(model)) { return; }
 
 			JsonObject payload = new JsonObject();
 			payload["model_name"] = model;
 			payload["parents"] = new JsonObject();
-			payload["gene_values"] = new JsonObject();
+			payload["gene_values"] = new JsonObject("chaos", .7);
 			payload["method"] = "mate";
 
-			Request.Post("https://artbreeder.com/tmp_compose_images", payload.ToString(), (id) => {
+			Request.Post(tempComposeURL, payload.ToString(), (id) => {
 				//Save(model, result);
-				Log.Info($"Got ID {id} from {model}");
+				Log.Info($"Generated new image ID {id} from {model}");
 				id = id.Replace("\"", "");
 				SaveImage(model, id);
 				Save(model, id);
 			}, Prepare);
-
 		}
 
-		public static void GenerateChild(string model, string parent, float style = .5f, float content = .5f, float chaos = .6f) {
+		/// <summary> Generate a child of a given image, within a given model. </summary>
+		/// <param name="model"> Model to generate with </param>
+		/// <param name="parent"> ID of parent </param>
+		/// <param name="style"> Ratio of style to keep (0, 1) </param>
+		/// <param name="content"> Ratio of content to keep (0, 1) </param>
+		/// <param name="chaos"> Craziness parameter. Range depends on model. </param>
+		public static void GenerateChild(string model, string parent, double style = .5f, double content = .5f, double chaos = .6f) {
 			if (!models.ContainsKey(model)) { return; }
+			JsonObject info = models[model] as JsonObject;
+			JsonObject magnitudes = info["magnitudes"] as JsonObject;
+			JsonArray chaosRange = magnitudes["chaos"] as JsonArray;
+			
+			chaos = Clamp(chaos, chaosRange[0].floatVal, chaosRange[1].floatVal);
+			style = Clamp01(style);
+			content = Clamp01(content);
 
 			JsonObject payload = new JsonObject();
 			payload["model_name"] = model;
@@ -123,9 +154,9 @@ namespace Ex {
 			payload["gene_values"] = new JsonObject("chaos", chaos);
 			payload["method"] = "mate";
 
-			Request.Post("https://artbreeder.com/tmp_compose_images", payload.ToString(), (id) => {
+			Request.Post(tempComposeURL, payload.ToString(), (id) => {
 				//Save(model, result);
-				Log.Info($"Got ID {id} from {model}");
+				Log.Info($"Single child of {parent}, child is ID {id} from {model}");
 				id = id.Replace("\"", "");
 				SaveImage(model, id);
 				Save(model, id, parent);
@@ -133,7 +164,49 @@ namespace Ex {
 
 		}
 
+		/// <summary> Breed 2 images together </summary>
+		/// <param name="model"> Model to use </param>
+		/// <param name="parentA"> ID of first parent </param>
+		/// <param name="parentB"> ID of second parent </param>
+		/// <param name="styleA"> Ratio of style of first parent </param>
+		/// <param name="contentA"> Ratio of content of first parent </param>
+		/// <param name="styleB"> Ratio of style of second parent </param>
+		/// <param name="contentB"> Ratio of content of second parent </param>
+		/// <param name="chaos"> Craziness parameter </param>
+		public static void Breed(string model, string parentA, string parentB,
+				double styleA = .5f, double contentA = .5f, double styleB = .5f, double contentB = .5f, double chaos = .6f) {
+			if (!models.ContainsKey(model)) { return; }
 
+			JsonObject info = models[model] as JsonObject;
+			JsonObject magnitudes = info["magnitudes"] as JsonObject;
+			JsonArray chaosRange = magnitudes["chaos"] as JsonArray;
+
+			chaos = Clamp(chaos, chaosRange[0].floatVal, chaosRange[1].floatVal);
+			styleA = Clamp01(styleA);
+			styleB = Clamp01(styleB);
+			contentA = Clamp01(contentA);
+			contentB = Clamp01(contentB);
+
+			JsonObject payload = new JsonObject();
+			payload["model_name"] = model;
+			payload["parents"] = new JsonObject(parentA, new JsonArray(styleA, contentA),
+												parentB, new JsonArray(styleB, contentB));
+			payload["gene_values"] = new JsonObject("chaos", chaos);
+			payload["method"] = "mate";
+
+			Request.Post(tempComposeURL, payload.ToString(), (id)=>{
+
+				Log.Info($"Bred {parentA} & {parentB}, child is ID {id} from {model}");
+				id = id.Replace("\"", "");
+				SaveImage(model, id);
+				Save(model, id, parentA, parentB);
+			}, Prepare);
+
+		}
+
+		/// <summary> Save an image from Artbreeder's temporary locations. </summary>
+		/// <param name="model"> Model that holds the image (used to place it in a local folder) </param>
+		/// <param name="id"> Image ID </param>
 		public static void SaveImage(string model, string id) {
 			string dir = $"./public/{model}";
 			if (!Directory.Exists(dir)) {
@@ -146,13 +219,19 @@ namespace Ex {
 			});
 		}
 
-
-		public static void Save(string model, string id, string parent = null) {
+		/// <summary> Tell art breeder to save an image for us. </summary>
+		/// <param name="model"> Model that is being used </param>
+		/// <param name="id"> ID of image to save </param>
+		/// <param name="parent"> Optional parent to allow artbreeder to record history.</param>
+		public static void Save(string model, string id, params string[] parentIDs) {
 			if (!models.ContainsKey(model)) { return; }
+
 
 			JsonObject payload = new JsonObject();
 			JsonArray parents = new JsonArray();
-			if (parent != null) { parents.Add(parent); }
+			if (parentIDs != null && parentIDs.Length > 0) { 
+				parents.AddAll(parentIDs);
+			}
 			payload["key"] = id;
 			payload["parent_keys"] = parents;
 			payload["model_name"] = model;

6f3d8c19ac6c7304aeba3042d0e2ee96a413e171
ninjapretzel <ninjapretzel@yahoo.com>
2020-11-21 15:41:24 -0700

    Re-enables tests, uses socket for testing (LIGHTNING FAST CONNECTION YAY)

--- Core/ServerTests.cs
+++ Core/ServerTests.cs
@@ -7,17 +7,20 @@ using UnityEngine;
 // For whatever reason, unity doesn't like mongodb, so we have to only include it server-side.
 #if !UNITY
 using Ex;
+using Ex.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using static Ex.EntityService;
 
 // TODO: Come back at some point and figure out why these tests cause stack overflows...
 
-public static class Server_Tests_Ignore {
+public static class Server_Tests {
 
 	private class TestData {
 		public Server server { get; private set; }
@@ -28,13 +31,39 @@ public static class Server_Tests_Ignore {
 		}
 
 	}
+	private class TestUserEntityInfo : DefaultUserEntityInfo { }
 
 	private static TestData DefaultSetup(string testDbName = "Testing", string testDb = "db", int port = 12345, float tick = 50) {
 		Server server = new Server(port, tick);
-		server.AddService<DebugService>();
-		server.AddService<LoginService>();
-		server.AddService<EntityService>();
-		server.AddService<MapService>();
+		var debug = server.AddService<DebugService>();
+		var login = server.AddService<LoginService>();
+		var entity = server.AddService<EntityService>();
+		var map = server.AddService<MapService>();
+		var db = server.AddService<DBService>()
+			.Connect()
+			.UseDatabase(testDbName)
+			.CleanDatabase()
+			;
+
+		// Note: A data type for holding user entity info must be registered.
+		// This effectively saves the user's information of where they are,
+		// and typically should be customized to either store all of a user's primary information, or links to them.
+		entity.RegisterUserEntityInfo<TestUserEntityInfo>();
+
+		// Note: Next, some logic should be provided so that when a user logs in for the first time,
+		// their data can be initialized to some default state.
+		login.userInitializer = (guid) => {
+			// In here, we can initialize anything the user needs.
+			// One of those things should be whatever was registered with `RegisterUserEntityInfo`.
+			var info = db.Initialize<TestUserEntityInfo>(guid, it => {
+				it.map = "HelloWorld";
+				it.position = Vector3.zero;
+				it.rotation = Vector3.zero;
+				it.skin = "Yeet";
+				it.color = "0xFFFFFFFF";
+			});
+		};
+
 
 		var serverSync = server.AddService<SyncService>();
 		{
@@ -49,15 +78,13 @@ public static class Server_Tests_Ignore {
 
 		}
 
-		server.AddService<DBService>()
-			.Connect()
-			.UseDatabase(testDbName)
-			.CleanDatabase()
-			;
 		server.Start();
 		Thread.Sleep(50);
 
-		Client admin = new Client(new TcpClient("localhost", port));
+		Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+		sock.Connect(IPAddress.Parse("127.0.0.1"), port);
+		Client admin = new Client(sock);
+		// Client admin = new Client(new TcpClient("localhost", port));
 		admin.AddService<DebugService>();
 		admin.AddService<LoginService>();
 		admin.AddService<EntityService>();
@@ -82,31 +109,19 @@ public static class Server_Tests_Ignore {
 
 	private static void CleanUp(TestData data) {
 		Log.Info("Waiting...");
-		Thread.Sleep(500);
+		Thread.Sleep(300);
 		Log.Info("Cleaning Up NOW.");
 		data.admin.server.Stop();
 		data.server.Stop();
 	}
 
-	public static void Test_Integrated_Generator() {
+	public static void TestSetup() {
 
 		var testData = DefaultSetup();
 		// defer CleanUp(testData);
 		try {
 
-
-			var db = testData.server.GetService<DBService>();
-			var data = db.GetData("Content", "ItemGeneration", "filename", "Materials");
-		
-			var gen = new Generator(data);
-		
-			GenSeed igSeed_1_0 = new GenSeed(Guid.Parse("3f162ba3-d167-4888-a26b-c193615e2af1"));
-			GenSeed igSeed_2_0 = new GenSeed(Guid.Parse("819f5900-9794-4d64-9aa9-e45e0dae9cde"));
-			GenSeed igSeed_2_1 = igSeed_2_0.Next();
-
-			var result = gen.Generate("Mineral", igSeed_1_0);
-
-			// Console.WriteLine(result.PrettyPrint());
+			
 
 		} finally {
 

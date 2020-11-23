6064ac83166cb9523ce06b334b5c84eb53a5c019
ninjapretzel <ninjapretzel@yahoo.com>
2020-11-23 15:17:05 -0700

    Makes all request methods return `Task`s, callbacks are now optional

--- Core/Utils/Request.cs
+++ Core/Utils/Request.cs
@@ -17,39 +17,43 @@ namespace Ex {
 		/// <summary> Attempt to GET the given URL and handle the response as a <see cref="string"/></summary>
 		/// <param name="url"> URL to GET from </param>
 		/// <param name="callback"> Callback to fire on success </param>
-		public static void Get(string url, Action<string> callback) {
-			Task.Run(async () => { await GetAsync(url, callback); });
+		/// <returns> Task that can be awaited for the completion. </returns>
+		public static Task<string> Get(string url, Action<string> callback = null) {
+			return Task.Run(()=>GetAsync(url, callback));
 		}
 		/// <summary> Attempt to GET the given URL and handle the response as a <see cref="string"/></summary>
 		/// <param name="url"> URL to GET from </param>
 		/// <param name="callback"> Callback to fire on success </param>
 		/// <returns> Awaitable <see cref="Task"/> </returns>
-		public static async Task GetAsync(string url, Action<string> callback) {
+		public static async Task<string> GetAsync(string url, Action<string> callback = null) {
 			try {
 				HttpResponseMessage response = await http.GetAsync(url);
 
-				await Finish(response, callback);
+				return await Finish(response, callback);
 			} catch (Exception e) {
-				onError?.Invoke($"Exception during GET raw", e);
+				onError?.Invoke($"Exception during GET", e);
+				return null;
 			}
 		}
 		/// <summary> Attempt to GET the given URL and handle the response as a <see cref="byte[]"/></summary>
 		/// <param name="url"> URL to GET from </param>
 		/// <param name="callback"> Callback to fire on success </param>
-		public static void GetRaw(string url, Action<byte[]> callback) {
-			Task.Run( async ()=>{ await GetRawAsync(url, callback); });
+		/// <returns> Task that can be awaited for the completion. </returns>
+		public static Task<byte[]> GetRaw(string url, Action<byte[]> callback) {
+			return Task.Run(()=>GetRawAsync(url, callback));
 		}
 		/// <summary> Attempt to GET the given URL and handle the response as a <see cref="byte[]"/></summary>
 		/// <param name="url"> URL to GET from </param>
 		/// <param name="callback"> Callback to fire on success </param>
 		/// <returns> Awaitable <see cref="Task"/> </returns>
-		public static async Task GetRawAsync(string url, Action<byte[]> callback) {
+		public static async Task<byte[]> GetRawAsync(string url, Action<byte[]> callback) {
 			try {
 				HttpResponseMessage response = await http.GetAsync(url);
 				
-				await Finish(response, callback);
+				return await Finish(response, callback);
 			} catch (Exception e) {
 				onError?.Invoke($"Exception during GET raw", e);
+				return null;
 			}
 		}
 
@@ -60,8 +64,8 @@ namespace Ex {
 		/// <param name="prep"> Custom generator of C#'s retarded <see cref="HttpContent"/>, if you want a different encoding or "ContentType"</param>
 		/// <returns> Awaitable Task </returns>
 		/// <remarks> Invokes <see cref="onError"/> during any failures. </remarks>
-		public static void Post(string url, string content, Action<string> callback, Func<string, HttpContent> prep = null) {
-			Task.Run( async ()=>{ await PostAsync(url, content, callback, prep); } );
+		public static Task<string> Post(string url, string content, Action<string> callback, Func<string, HttpContent> prep = null) {
+			return Task.Run(()=>PostAsync(url, content, callback, prep));
 		}
 
 		/// <summary> Attempt to POST to the given URL and handle the response as a <see cref="string"/> </summary>
@@ -71,40 +75,44 @@ namespace Ex {
 		/// <param name="prep"> Custom generator of C#'s retarded <see cref="HttpContent"/>, if you want a different encoding or "ContentType", or any custom headers. </param>
 		/// <returns> Awaitable Task </returns>
 		/// <remarks> Invokes <see cref="onError"/> during any failures. </remarks>
-		public static async Task PostAsync(string url, string content, Action<string> callback, Func<string, HttpContent> prep = null) {
+		public static async Task<string> PostAsync(string url, string content, Action<string> callback, Func<string, HttpContent> prep = null) {
 			try {
 				HttpContent request;
 
 				if (prep == null) {
 					request = new StringContent(content, Encoding.UTF8, "application/json");
-
 				} else {
 					request = prep(content);
 				}
 
 				HttpResponseMessage response = await http.PostAsync(url, request);
-				await Finish(response, callback);
+				return await Finish(response, callback);
 			} catch (Exception e) {
 				onError?.Invoke($"Exception during POST", e);
+				return null;
 			}
 		}
 
 
-		private static async Task Finish(HttpResponseMessage response, Action<string> callback) {
+		private static async Task<string> Finish(HttpResponseMessage response, Action<string> callback = null) {
 			if (response.IsSuccessStatusCode) {
 				string result = await response.Content.ReadAsStringAsync();
-				callback(result);
+				callback?.Invoke(result);
+				return result;
 			} else {
 				onError?.Invoke($"Bad status code from {response.RequestMessage.Method}", null);
+				return null;
 			}
 		}
 
-		private static async Task Finish(HttpResponseMessage response, Action<byte[]> callback) {
+		private static async Task<byte[]> Finish(HttpResponseMessage response, Action<byte[]> callback = null) {
 			if (response.IsSuccessStatusCode) {
 				byte[] result = await response.Content.ReadAsByteArrayAsync();
-				callback(result);
+				callback?.Invoke(result);
+				return result;
 			} else {
 				onError?.Invoke($"Bad status code from {response.RequestMessage.Method}", null);
+				return null;
 			}
 		}
 	}

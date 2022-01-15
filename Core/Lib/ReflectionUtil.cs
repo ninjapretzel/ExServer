122a23f00463e2b171c924cc74ba9ad43be289ed
ninjapretzel <ninjapretzel@yahoo.com>
2022-01-15 15:29:45 -0700

    Adds a function to search globally for all types in any namespace

---
+++ Core/Lib/ReflectionUtil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex.Utils {
+	public static class ReflectionUtil {
+
+		public static Type FindType(string name) {
+			Type t = null;
+			foreach (var a in AppDomain.CurrentDomain.GetAssemblies()) {
+				t = a.GetType(name);
+				if (t != null) { return t; }
+			}
+			return null;
+		}
+	}
+}

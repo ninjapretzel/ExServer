892a25a5bc2225c19e650264a4d814238c10d1ec
ninjapretzel <ninjapretzel@yahoo.com>
2022-02-23 19:05:37 -0700

    Formats test better

--- Core/Lib/Incremental.cs
+++ Core/Lib/Incremental.cs
@@ -46,24 +46,24 @@ namespace Ex.Libs {
 
 	public static class zzIncremental_Tests {
 		public static void TestThing() {
-			Incremental.Format(1).ShouldBe("1.00");
-			Incremental.Format(10).ShouldBe("10.00");
-			Incremental.Format(100).ShouldBe("100.00");
-			Incremental.Format(1000).ShouldBe("1.00 K");
-			Incremental.Format(10000).ShouldBe("10.00 K");
-			Incremental.Format(100000).ShouldBe("100.00 K");
-			Incremental.Format(1000000).ShouldBe("1.00 M");
-			Incremental.Format(5050000).ShouldBe("5.05 M");
-			Incremental.Format(50500000).ShouldBe("50.50 M");
-			Incremental.Format(505000000).ShouldBe("505.00 M");
-			Incremental.Format(5050000000).ShouldBe("5.05 B");
-			Incremental.Format(50500000000).ShouldBe("50.50 B");
-			Incremental.Format(505000000000).ShouldBe("505.00 B");
-			Incremental.Format(5050000000000).ShouldBe("5.05 T");
-			Incremental.Format(50500000000000).ShouldBe("50.50 T");
-			Incremental.Format(505000000000000).ShouldBe("505.00 T");
-			Incremental.Format(100000000000000).ShouldBe("100.00 T");
-			Incremental.Format(1000000000000000).ShouldBe("1.00 Qa");
+			Incremental.Format(                    1).ShouldBe("1.00");
+			Incremental.Format(                   10).ShouldBe("10.00");
+			Incremental.Format(                  100).ShouldBe("100.00");
+			Incremental.Format(                1_000).ShouldBe("1.00 K");
+			Incremental.Format(               10_000).ShouldBe("10.00 K");
+			Incremental.Format(              100_000).ShouldBe("100.00 K");
+			Incremental.Format(            1_000_000).ShouldBe("1.00 M");
+			Incremental.Format(            5_050_000).ShouldBe("5.05 M");
+			Incremental.Format(           50_500_000).ShouldBe("50.50 M");
+			Incremental.Format(          505_000_000).ShouldBe("505.00 M");
+			Incremental.Format(        5_050_000_000).ShouldBe("5.05 B");
+			Incremental.Format(       50_500_000_000).ShouldBe("50.50 B");
+			Incremental.Format(      505_000_000_000).ShouldBe("505.00 B");
+			Incremental.Format(    5_050_000_000_000).ShouldBe("5.05 T");
+			Incremental.Format(   50_500_000_000_000).ShouldBe("50.50 T");
+			Incremental.Format(  505_000_000_000_000).ShouldBe("505.00 T");
+			Incremental.Format(  100_000_000_000_000).ShouldBe("100.00 T");
+			Incremental.Format(1_000_000_000_000_000).ShouldBe("1.00 Qa");
 
 		}
 	}
